232125a2245b76a094de32a1d0adf8add4e6b352
Lor3ttox <juan.dev1984@gmail.com>
2025-12-19 06:38:05 +0000

    Codigo para guardar las tablas dentro del Unity Catalog para el linage de tablas

--- configs/utils/functions.py
+++ configs/utils/functions.py
@@ -9,3 +9,15 @@ from pyspark.sql import Row, DataFrame
 import pandas as pd
 import numpy as np
 import sklearn as sk
+
+#-------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------
+def save_table_unity(df, table_name, catalogo = "dataknow_dll", schema = "construccion"):
+    """
+    Funcion para guardar un dataframe en la unidad de almacenamiento de Databricks
+    """
+    tabla_destino = f"{catalogo}.{schema}.{table_name}"
+    df.write \
+    .mode("overwrite") \
+    .option("overwriteSchema", "true") \
+    .saveAsTable(tabla_destino)
